628bcd3b17ce87be3702369ced093d30546bbf0c
Karl Ragnar Giese <karl@giese.no>
2024-12-29 04:08:00 +0100

    Improved C support

--- cerberus-c.el
+++ cerberus-c.el
@@ -28,24 +28,50 @@
 
 (cerberus-lang-def-things
  'c
- `((cerberus-sentence
-    ,(regexp-opt '("preproc"
-		   "declaration"
-		   ;; "specifier"
-		   "attributed_statement"
-		   "labeled_statement"
-		   "expression_statement"
-		   "if_statement"
-		   "switch_statement"
-		   "do_statement"
-		   "while_statement"
-		   "for_statement"
-		   "return_statement"
-		   "break_statement"
-		   "continue_statement"
-		   "goto_statement"
-		   "case_statement")))
-   
+ `((cerberus-condition
+    ,(lambda (node) (and (treesit-node-check node 'named)
+		    (equal "condition" (treesit-node-field-name (treesit-node-parent node))))))
+   (cerberus-sentence
+    (,(regexp-opt '("attributed_statement"
+		    "break_statement"
+		    "case_statement"
+		    "comment"
+		    "continue_statement"
+		    "declaration"
+		    "do_statement"
+		    "else_clause"
+		    "expression_statement"
+		    "for_statement"
+		    "function_definition"
+		    "goto_statement"
+		    "if_statement"
+		    "labeled_statement"
+		    "preproc_def"
+		    "preproc_include"	; Mostly yanked from c-ts-mode
+		    "return_statement"
+		    "switch_statement"
+		    "while_statement")
+		  'symbols)
+     .
+     ;; Dont include [else if] if-statements
+     ,(lambda (node) (not (and (cerberus--node-is-thing-p (treesit-node-parent node) "^else_clause$")
+			  (cerberus--node-is-thing-p node "^if_statement$"))))))
+
+   (cerberus-word
+    ,(lambda (node)
+       (and
+	;; Must not have (smaller) child nodes
+	(zerop (cerberus--node-smaller-child-count node t))
+	(or
+	 ;; And either be named,
+	 (treesit-node-check node 'named)
+	 ;; an operator,
+	 (equal "operator" (treesit-node-field-name node))
+	 ;; or one of theese:
+	 (string-match (concat "\\`"
+			       (regexp-opt '("=" "*" "&" "struct"))
+			       "\\'")
+		       (treesit-node-text node t))))))
    (cerberus-nontrailing-list
     ,(regexp-opt '("argument_list"
 		   "parameter_list"
